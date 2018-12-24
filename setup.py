f9243c22d9fe1b9d8bdcf4bd44a3d714f2ca17ef
timoryxl <tim-xl@hotmail.com>
2018-12-23 22:03:00 -0800

    add setup files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages()
+
+setup(name='MLScratch',
+      version='0.1',
+      description='Interview practice',
+      author='Lin Xiao',
+      author_email='tim-xl@hotmail.com',
+      packages=find_packages())
