6cf9659ed586530ec065d703523f71b68e95e579
timoryxl <tim-xl@hotmail.com>
2018-12-26 23:11:04 -0800

    Add gini entropy for impurity

--- algorithms/decisiontree.py
+++ algorithms/decisiontree.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def class_counts(rows, target_index):
     counts = dict()
     for row in rows:
@@ -24,3 +27,26 @@ class Question:
                                                                    condition=condition,
                                                                    value=self.value)
 
+
+def partition(rows, question):
+    true_rows, false_rows = [], []
+    for row in rows:
+        if question.match(row):
+            true_rows.append(row)
+        else:
+            false_rows.append(row)
+    return true_rows, false_rows
+
+
+def impurity(rows, metric):
+    counts = class_counts(rows)
+    value = 1
+    if metric.lower() == 'gini':
+        for label in counts:
+            p = counts[label]/float(len(rows))
+            value -= p**2
+    elif metric.lower() == 'entropy':
+        for label in counts:
+            p = counts[label]/float(len(rows))
+            value -= p**np.log2(p)
+    return value
